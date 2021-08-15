e7a084a069e74b57fe971eb2de4bc05b9734b85a
sloth-hobby <shotiku1112@gmail.com>
2021-08-15 20:30:57 +0900

    fixed_name

---
+++ lstm_practice/predict_simple_formula_train.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class PredictSimpleFormulaNet(nn.Module):
+    def __init__(self, inputDim, hiddenDim, outputDim):
+        super(Predictor, self).__init__()
+
+        self.rnn = nn.LSTM(input_size = inputDim,
+                            hidden_size = hiddenDim,
+                            batch_first = True)
+        self.output_layer = nn.Linear(hiddenDim, outputDim)
+
+    def forward(self, inputs, hidden0=None):
+        output, (hidden, cell) = self.rnn(inputs, hidden0)
+        output = self.output_layer(output[:, -1, :])
+
+        return output

34576f4c31499663a1e3ce0ae549fe032c958c52
sloth-hobby <shotiku1112@gmail.com>
2021-08-18 01:09:32 +0900

    Implement_train_function_but_error

--- lstm_practice/predict_simple_formula_train.py
+++ lstm_practice/predict_simple_formula_train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import numpy as np
@@ -30,9 +31,14 @@ class Train():
         num_layers = 1
         batch_first = True
         dropout = 0
+
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         print("device：", self.device)
         self.net = PredictSimpleFormulaNet(input_size, output_size, hidden_size, num_layers, batch_first, dropout).to(self.device)
+        self.criterion = nn.MSELoss(reduction='mean')
+        self.optimizer = optim.Adam(self.net.parameters(),
+                                        lr=0.001,
+                                        betas=(0.9, 0.999), amsgrad=True)
 
     def make_dataset(self, dataset_num, input_length, t_start):
         dataset_inputs = []
@@ -43,6 +49,59 @@ class Train():
 
         return np.array(dataset_inputs), np.array(dataset_labels)
 
+    def train_step(self, inputs, labels):
+        print("inputs = {}, labels = {}".format(inputs.shape, labels.shape))
+        inputs = torch.Tensor(inputs).to(self.device)
+        labels = torch.Tensor(labels).to(self.device)
+        self.net.train()
+        preds = self.net(inputs)
+        loss = self.criterion(preds, labels)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss, preds
+
+    def test_step(x, t):
+        inputs = torch.Tensor(inputs).to(self.device)
+        labels = torch.Tensor(labels).to(self.device)
+        self.net.val()
+        preds = self.net(inputs)
+        loss = self.criterion(preds, labels)
+
+        return loss, preds
+
+    def train(self, train_inputs, train_labels, test_inputs, test_labels, epochs, batch_size):
+        torch.backends.cudnn.benchmark = True   # ネットワークがある程度固定であれば、高速化させる
+
+        n_batches_train = train_inputs.shape[0]
+        n_batches_test = test_inputs.shape[0]
+        for epoch in range(epochs):
+            print('-------------')
+            print('Epoch {}/{}'.format(epoch+1, epochs))
+            print('-------------')
+            train_loss = 0.
+            test_loss = 0.
+            train_inputs_shuffle, train_labels_shuffle = shuffle(train_inputs, train_labels)
+            for batch in range(n_batches_train):
+                start = batch * batch_size
+                end = start + batch_size
+                loss, _ = self.train_step(train_inputs_shuffle[start:end], train_labels_shuffle[start:end])
+                train_loss += loss.item()
+
+            for batch in range(n_batches_test):
+                start = batch * batch_size
+                end = start + batch_size
+                loss, _ = self.test_step(test_inputs[start:end], test_labels[start:end])
+                test_loss += loss.item()
+
+            train_loss /= n_batches_train
+            test_loss /= n_batches_test
+            print('loss: {:.3}, test_loss: {:.3f}'.format(
+                train_loss,
+                test_loss
+                ))
+
 if __name__ == '__main__':
     '''
     定数
@@ -50,6 +109,9 @@ if __name__ == '__main__':
     dataset_num = 100
     input_length = 50
     t_start = -100.0
+    # train pram
+    epochs = 1000
+    batch_size = 100
     '''
     学習用のデータセットを用意
     '''
@@ -58,4 +120,5 @@ if __name__ == '__main__':
     print("dataset_inputs = {}, dataset_labels = {}".format(dataset_inputs.shape, dataset_labels.shape))
     train_inputs, test_inputs, train_labels, test_labels = train_test_split(dataset_inputs, dataset_labels, test_size=0.2, shuffle=False)
     print("train_inputs = {}, train_labels = {}, test_inputs = {}, test_labels = {}".format(train_inputs.shape, train_labels.shape, test_inputs.shape, test_labels.shape))
+    train.train(train_inputs, train_labels, test_inputs, test_labels, epochs, batch_size)
 
